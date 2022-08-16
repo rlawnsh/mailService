a26e53c641d5c20622dd1446246941fbc67e8735
rlawnsh <wnshrla@naver.com>
2022-08-16 17:43:40 +0900

    FEAT: request parameter 분리

--- src/main/java/hello/mailService/controller/MailController.java
+++ src/main/java/hello/mailService/controller/MailController.java
@@ -1,5 +1,6 @@
 package hello.mailService.controller;
 
+import hello.mailService.config.SecurityConfig;
 import hello.mailService.dto.MailDto;
 import hello.mailService.service.Bucket4jService;
 import hello.mailService.service.MailService;
@@ -8,15 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -27,12 +26,27 @@ public class MailController {
     private final MailService mailService;
     private static final Bucket bucket = Bucket4jService.generateSimpleBucket();
 
+    @PostMapping(value = "/mail/send", consumes = "multipart/form-data")
+    public ResponseEntity<String> sendMail(@RequestParam String fromAddress,
+                                           @RequestParam("toAddress") List<String> toAddressList,
+                                           @RequestParam("ccAddress") List<String> ccAddressList,
+                                           @RequestParam String title,
+                                           @RequestParam String content,
+                                           @RequestParam(value = "images", required = false) List<MultipartFile> imgList,
+                                           @RequestParam(value = "files", required = false) List<MultipartFile> fileList,
+                                           HttpServletRequest request) throws MessagingException, IOException {
 
-    @PostMapping("/mail/send")
-    public ResponseEntity<String> sendMail(@RequestPart("mailInfo") MailDto mailDto, @RequestPart(value = "files", required = false) List<MultipartFile> fileList) throws MessagingException, IOException {
-
+        MailDto mailDto = new MailDto(fromAddress, toAddressList, ccAddressList, title, content);
+        HashMap<String, String> authToken = SecurityConfig.authToken;
         if (bucket.tryConsume(1)) {
             log.info(">>> Remain bucket count : {}", bucket.getAvailableTokens());
+            String authorization = request.getHeader("Authorization");
+            for (String key : authToken.keySet()) {
+                String value = authToken.get(key);
+                if (value.equals(authorization)) {
+                    log.info("보낸 서버는 : {}", key);
+                }
+            }
             mailService.sendMail(mailDto, fileList);
             return new ResponseEntity<String>("Ok", HttpStatus.OK);
         }
