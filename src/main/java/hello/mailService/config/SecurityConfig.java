e89f954d59c576b02393251b4cc7434ff7acbeb1
rlawnsh <wnshrla@naver.com>
2022-08-17 14:21:42 +0900

    FEAT: Spring Security

--- src/main/java/hello/mailService/config/SecurityConfig.java
+++ src/main/java/hello/mailService/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package hello.mailService.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,17 +14,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "http")
+@Slf4j
 public class SecurityConfig {
 
     @Value("${http.auth-token-header.name}")
     private String principalRequestHeader;
-
     public static HashMap<String, String> authToken;
 
     public HashMap<String, String> getAuthToken() {
@@ -40,6 +41,7 @@ public class SecurityConfig {
         filter.setAuthenticationManager(authentication -> {
             String principal = (String) authentication.getPrincipal();
             if (!authToken.containsValue(principal)) {
+                log.info("에러 발생");
                 throw new BadCredentialsException("The API key was not found or not the expected value");
             }
             authentication.setAuthenticated(true);
@@ -57,6 +59,7 @@ public class SecurityConfig {
                 .antMatchers("/index").permitAll()
                 .anyRequest()
                 .authenticated();
+
         return http.build();
     }
 }
